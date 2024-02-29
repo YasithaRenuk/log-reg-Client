40554011badd89e703d3256ffa92594e3845fb71
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2024-02-29 21:42:30 +0530

    small chenges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <Link class="navbar-brand" href="/">
+          Bodima
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <CustomLink to = '/'>Home</CustomLink>
+            <CustomLink to = '/register'>Register</CustomLink>
+            <CustomLink to= '/login'>Login</CustomLink>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <Link class={isActive ? "nav-link active" : "nav-link"} to ={to} {...props}>{children}</Link>
+  );
 }
